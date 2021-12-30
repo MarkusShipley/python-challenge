a5f291ba09822256204fd52df54cc28040456531
MarkusShipley <mrksshipley@gmail.com>
2021-12-30 17:51:34 -0500

    initial main file

---
+++ PyBank/Main.py
@@ -0,0 +1,8 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'accounting.csv')
